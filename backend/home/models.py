81e4214ee42f24c4862184d44ba41afd7715569d
Crowdbotics <engineering@crowdbotics.com>
2022-10-21 14:12:52 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+class Car(models.Model):
+    'Generated Model'
+    brand = models.CharField(max_length=100,)
+    model = models.CharField(max_length=100,)
+    year = models.IntegerField()
+    price = models.IntegerField()
+    description = models.TextField()
