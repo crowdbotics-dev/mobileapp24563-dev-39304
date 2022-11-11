36115737964aefed69b509a34340a624771e8280
Aline Tavares <aline@crowdbotics.com>
2022-11-11 12:14:43 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -1,9 +1,10 @@
+import { Text } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled4 = () => {
   return <View style={_styles.XGiTPmsN}>
-      </View>;
+      <View style={_styles.iaXwVPdj}><Text style={_styles.vVdBCNEB}>Order name</Text><Text style={_styles.vUFnctkx}>No. #0556465465</Text></View><View style={_styles.ToyPBQzF}><Text style={_styles.hbyNvMPU}>Order name</Text><Text style={_styles.PJdgIgQa}>No. #0556465465</Text></View><View style={_styles.aEXDKgLW}><Text style={_styles.AhDEMJOh}>Order name</Text><Text style={_styles.xqMFLxzL}>No. #0556465465</Text></View></View>;
 };
 
 export default Untitled4;
@@ -14,5 +15,41 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  iaXwVPdj: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#fff"
+  },
+  vVdBCNEB: {
+    fontSize: 22
+  },
+  vUFnctkx: {
+    color: "#7C7C7C",
+    fontSize: 14
+  },
+  ToyPBQzF: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#fff"
+  },
+  hbyNvMPU: {
+    fontSize: 22
+  },
+  PJdgIgQa: {
+    color: "#7C7C7C",
+    fontSize: 14
+  },
+  aEXDKgLW: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#fff"
+  },
+  AhDEMJOh: {
+    fontSize: 22
+  },
+  xqMFLxzL: {
+    color: "#7C7C7C",
+    fontSize: 14
   }
 });
